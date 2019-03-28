2fea98c58deadefe44637ad7fa948d5dfc4f1611
Carl Sziebert <csziebert@google.com>
2019-03-28 08:07:36 -0700

    Experimenting with some UI widgets

--- app/src/main/java/com/seymourapp/seymour/activity/main/MainActivity.java
+++ app/src/main/java/com/seymourapp/seymour/activity/main/MainActivity.java
@@ -1,15 +1,21 @@
 package com.seymourapp.seymour.activity.main;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.seymourapp.seymour.R;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 import dagger.android.ContributesAndroidInjector;
 import dagger.android.support.DaggerAppCompatActivity;
 
-public class MainActivity extends DaggerAppCompatActivity {
+public class MainActivity extends DaggerAppCompatActivity implements
+        BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private Toolbar toolbar;
 
     @dagger.Module
     public interface Module {
@@ -28,7 +34,18 @@ public class MainActivity extends DaggerAppCompatActivity {
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.inflateMenu(R.menu.main_bottom_nav_menu);
+        bottomNav.setOnNavigationItemSelectedListener(this);
+        bottomNav.setSelectedItemId(R.id.action_unread);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        toolbar.setTitle(item.getTitle());
+        return true;
     }
 }
--- app/src/main/java/com/seymourapp/seymour/application/SeymourApplication.java
+++ app/src/main/java/com/seymourapp/seymour/application/SeymourApplication.java
@@ -1,9 +1,21 @@
 package com.seymourapp.seymour.application;
 
+import com.seymourapp.seymour.BuildConfig;
+
 import dagger.android.AndroidInjector;
 import dagger.android.support.DaggerApplication;
+import timber.log.Timber;
 
 public class SeymourApplication extends DaggerApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+    }
 
     @Override
     protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
